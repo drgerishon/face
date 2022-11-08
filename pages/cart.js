4ec12711c59b861a076035557312b3dc4151cb47
drgerishon <gerishonkungu@gmail.com>
2022-11-08 08:47:54 +0300

    cart

--- pages/cart.js
+++ pages/cart.js
@@ -77,7 +77,7 @@ function CartScreen() {
                         ))}
                       </select>
                     </td>
-                    <td className="p-5 text-right">${item.price}</td>
+                    <td className="p-5 text-right">Ksh{item.price}</td>
                     <td className="p-5 text-center">
                       <button onClick={() => removeItemHandler(item)}>
                         <XCircleIcon className="h-5 w-5"></XCircleIcon>
@@ -92,7 +92,8 @@ function CartScreen() {
             <ul>
               <li>
                 <div className="pb-3 text-xl">
-                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) : Ksh
+                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) :
+                  Ksh
                   {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
                 </div>
               </li>
